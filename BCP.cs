5af5cc96a78cff173a020dbcb2cd263656ac2f79
ChristianSotoMartinez <71237998+ChristianSotoMartinez@users.noreply.github.com>
2021-06-29 19:02:19 -0500

    Add files via upload

---
+++ BCP.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace Act14_Paginacion
+{
+  public partial class BCP : Form
+  {
+    public BCP(Scheduler scheduler)
+    {
+      InitializeComponent();
+
+      foreach(Process p in scheduler.New) {
+        tblBCP.Rows.Add(p.ToStringAll());
+      }
+      foreach(Process p in scheduler.Ready) {
+        tblBCP.Rows.Add(p.ToStringAll());
+      }
+      if(scheduler.Running.State == States.Running) {
+        tblBCP.Rows.Add(scheduler.Running.ToStringAll());
+      }
+      foreach(Process p in scheduler.Blocked) {
+        tblBCP.Rows.Add(p.ToStringAll());
+      }
+      foreach(Process p in scheduler.Exit) {
+        tblBCP.Rows.Add(p.ToStringAll());
+      }
+    }
+  }
+}
